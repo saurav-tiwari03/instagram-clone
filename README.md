c1d75be65b499e3fdc20153537d9df262dcab950
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-01-20 05:29:16 +0530

    Day1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Instagram Clone Project
+
+## Overview
+This project aims to replicate the functionality of Instagram, utilizing React for front-end development and Firebase for both authorization and backend services.
+
+## Project Updates
+I am committed to providing regular updates to this repository, detailing the progress made on a daily basis.
+
+## Day 1:
+ - Established the project's routing structure.
+ - Implemented an authorization modal to manage user authentication.
---
+++ src/config/firsbase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGvyebVxly6b8tdMjEmqOnlJohmJ4r7vY",
+  authDomain: "emailpasswordlogin-ae0ad.firebaseapp.com",
+  projectId: "emailpasswordlogin-ae0ad",
+  storageBucket: "emailpasswordlogin-ae0ad.appspot.com",
+  messagingSenderId: "812893136140",
+  appId: "1:812893136140:web:995f8e6ea6f12e33f25529",
+  measurementId: "G-0LYTMWVVWN"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getAuth(app);
